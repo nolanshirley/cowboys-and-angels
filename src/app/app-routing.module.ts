bf08de442934d8239db4a8ee8063b436b2989b4d
Nolan Shirley <nolanbshirley@gmail.com>
2022-02-28 19:58:50 -0500

    components and styles built out

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactComponent } from './contact/contact.component';
+import { MainComponent } from './main/main.component';
+import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
+import { StylistsComponent } from './stylists/stylists.component';
+
+const routes: Routes = [{
+  path: '',
+  component: MainComponent,
+  children: [
+    { path: '', redirectTo: 'stylists', pathMatch: 'full'},
+    { path: 'products', component: ProductsComponent, data: {animation: 'productsPage'}}, 
+    { path: 'services', component: ServicesComponent, data: {animation: 'servicesPage'} },
+    { path: 'stylists', component: StylistsComponent, data: {animation: 'stylistsPage'}},
+    { path: 'contact', component: ContactComponent, data: { animation: 'contactPage'} }
+  ]
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
+import { StylistsComponent } from './stylists/stylists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ContactComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductsComponent,
+    ServicesComponent,
+    StylistsComponent
   ],
   imports: [
     BrowserModule,
